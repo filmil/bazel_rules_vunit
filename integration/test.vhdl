80b4303216b2f4e37f426547d9a32ac68f6f739f
Filip Filmar <filmil@gmail.com>
2026-01-21 06:10:42 +0000

    feat: add some more logging examples

--- integration/test.vhdl
+++ integration/test.vhdl
@@ -2,13 +2,19 @@ library vunit_lib;
 context vunit_lib.vunit_context;
 
 entity tb is
+    generic(
+        runner_cfg: string := runner_cfg_default
+    );
 end entity;
 
 architecture sim of tb is
 begin
     process
+        variable logger : logger_t := get_logger("test/UART");
     begin
-        info("Hello world!");
-        std.env.finish;
+        test_runner_setup(runner, runner_cfg);
+        info(logger, "Hello world!");
+        check_true(true, "oops");
+        test_runner_cleanup(runner);
     end process;
 end architecture;
