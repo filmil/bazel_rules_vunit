f2e16022aa7cbf4b598bc9caf78e2414072b7ffd
Filip Filmar <filmil@gmail.com>
2026-06-15 01:35:18 +0000

    feat: add VHDL standard flag support and selective compilation
    - Introduced the vhdl_standard build setting supporting 1993, 2002, 2008, and 2019 standard versions.
    - Added selective compilation inside vunit.BUILD.bazel and third_party/vunit/BUILD.bazel to conditionally exclude/include files based on standard (e.g., -2008p.vhd, context files, random, and com libraries).
    - Configured integration test suite to select correct test files and dependencies according to the active VHDL standard.
    - Updated GitHub Actions build workflow to test across all matrix configurations for the vhdl_standard.

---
+++ integration/test_93_2002.vhdl
@@ -0,0 +1,47 @@
+library vunit_lib;
+use vunit_lib.check_pkg.all;
+use vunit_lib.checker_pkg.all;
+use vunit_lib.logger_pkg.all;
+use vunit_lib.run_pkg.all;
+use vunit_lib.runner_pkg.all;
+use vunit_lib.run_types_pkg.all;
+
+entity tb is
+    generic(
+        --! Default vunit runner configuration.
+        runner_cfg: string := runner_cfg_default
+    );
+end entity;
+
+architecture sim of tb is
+begin
+    process
+        variable logger : logger_t := get_logger("test/UART");
+    begin
+        test_runner_setup(runner, runner_cfg);
+        while test_suite loop
+            if run("a") then
+                info(logger, "Hello world!");
+                check_true(true, "oops");
+            end if;
+            if run("b") then
+                info(logger, "Hello world 2!");
+                check_true(true, "oops");
+            end if;
+        end loop;
+        test_runner_cleanup(runner);
+    end process;
+end architecture;
+
+architecture sim2 of tb is
+begin
+    process begin
+        test_runner_setup(runner, runner_cfg);
+        check_true(true, "oops");
+        test_runner_cleanup(runner);
+    end process;
+end architecture;
+
+configuration tb_cfg_default of tb is
+    for sim end for;
+end configuration;
